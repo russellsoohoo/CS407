37addada6c0a81fb2e6e30c36a249c5111ee9785
russellsoohoo <russell.soohoo@gmail.com>
2024-10-29 22:39:07 -0700

    initial calendar and event database
    Created the initial calendar view. Created the 'model' for an event database to be added to the calendar

--- saac_schedule_proj/saac_app/models.py
+++ saac_schedule_proj/saac_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Event(models.Model):
+    SUBCOMMITTEES = {
+        "bo": "BeOregon",
+        "au": "Athlete Unification",
+        "dts": "Duck The Stigma",
+        "oh": "OHeroes",
+        "woo": "Women Of Oregon",
+        "s": "SAAC",
+        "dgp": "Ducks Go Pro",
+    }
+    name = models.CharField(max_length=100) # name of event
+    description = models.TextField() # description of event
+    date = models.DateTimeField()  # date of event
+    subcommittee = models.CharField(max_length=3, choices=SUBCOMMITTEES) # subcommittee of event
+
+    def __str__(self):
+        return self.name
--- saac_schedule_proj/saac_app/views.py
+++ saac_schedule_proj/saac_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Event
+from django.http import JsonResponse
 
-# Create your views here.
+def home(request):
+    events = Event.objects.all()
+    return render(request, 'home.html', {'events': events})
+
+def calendar(request):
+    events = Event.objects.all()
+    return render(request, 'calendar.html', {'events': events})
+
+def events_api(request):
+    events = Event.objects.all()
+    events_list = []
+
+    for event in events:
+        events_list.append({
+            "name": event.name,
+            "desc": event.description,
+            "date": event.date.isoformat(),
+            "subc": event.subcommittee,
+        })
+
+    return JsonResponse(events_list, safe=False)
